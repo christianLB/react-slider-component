06ab43fd8b9a719e7bd2dd8f3b638a2a9be9b1f1
Aguero <cnao@gft.com>
2019-02-04 12:57:38 +0100

    initial commit

---
+++ src/range-slider/range-slider.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class RangeSlider extends Component {
+  constructor(props){
+    super(props);
+    this.track = React.createRef();
+    this.val = React.createRef();
+    this.style = {
+      display: 'grid',
+      placeItems: 'center'
+    }
+  }
+
+  render() {
+    const {props, track, val, style} = this;
+    const {background, width, height, value} = props;
+    return <div
+      id={'rangeSlider'}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        backgroundColor: background,
+        ...style,
+      }}
+      >
+        <span ref={track} style={{position: 'absolute'}}></span>
+        <span ref={val}>{value}</span>
+    </div>;
+  }
+}
+
+RangeSlider.defaultProps = {
+    width: '300',
+    height: '30',
+    value: 10,
+    background: '#D2D2D2',
+}
+
+export default RangeSlider;
